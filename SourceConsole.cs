b0257530c0fc99dc2fae857f7bcf3806fa0248a6
jonathan-airey-mw <jairey@merchantwarehouse.com>
2013-04-26 12:39:30 -0400

    inital commit

---
+++ SourceConsole.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Smoking_Gun
+{
+    public class SourceConsole
+    {
+        private IWebElement _bonusElement;
+        private IWebElement _approvalWeightElement;
+        private IWebElement _sourceElement;
+        private readonly IWebElement _row;
+        public SourceConsole(IWebElement row)
+        {
+            _row = row;
+            SetMembers();
+        }
+        private void SetMembers()
+        {
+            try
+            {
+            string sourceId = _row.FindElement(By.CssSelector("span")).GetAttribute("id");
+            SetSourceElement(sourceId);
+            SetBonusElement(sourceId);
+            SetApprovalWeight(sourceId);
+            }
+            catch (Exception)
+            {
+  
+            }
+        }
+        private void SetSourceElement(string sourceId)
+        {
+            _sourceElement = _row.FindElement(By.Id(sourceId));
+        }
+        private void SetBonusElement(string sourceId)
+        {
+            _bonusElement = _row.FindElement(By.Id(sourceId.Replace("lblSourceName", "lblBonus")));
+        }
+        private void SetApprovalWeight(string sourceId)
+        {
+            _approvalWeightElement = _row.FindElement(By.Id(sourceId.Replace("lblSourceName", "lblApprovalWeight")));
+        }
+        public bool IsInvalidRow()
+        {
+            return (_sourceElement != null && _bonusElement != null && _approvalWeightElement != null && _approvalWeightElement.Text != "1");
+        }
+        public string Source
+        {
+            get { return _sourceElement.Text; }
+        }
+        public string ApprovalWeight
+        {
+            get { return _approvalWeightElement.Text; }
+        }
+        public string Bonus
+        {
+            get { return _bonusElement.Text; }
+        }
+    }
+}
---
+++ TestingClass.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+namespace Smoking_Gun
+{
+    public class TestingClass
+    {
+        private IWebDriver _driver;
+        private string _baseUrl;
+        
+
+        [SetUp]
+        public void SetupTest()
+        {
+            _driver = new InternetExplorerDriver();
+            _baseUrl = ConfigurationManager.AppSettings["baseURL"];
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+                _driver.Quit();
+        }
+
+        [Test]
+        public void BadTest()
+        {
+            string url = _baseUrl + ConfigurationManager.AppSettings["webPage"];
+            _driver.Navigate().GoToUrl(_baseUrl + "/OpsCenter/SourceConsole.aspx");
+            IWebElement table = _driver.FindElement(By.CssSelector("table.specialClass"));
+            List<SourceConsole> listSourceConsole = table.FindElements(By.CssSelector("tr")).Select(row => new SourceConsole(row)).Where(sourceConsole => sourceConsole.IsInvalidRow()).ToList();
+            PrintInvalidRows(listSourceConsole);
+            Assert.AreEqual(0, listSourceConsole.Count);
+        }
+
+        [Test]
+        public void BetterTest()
+        {
+            string url = _baseUrl + ConfigurationManager.AppSettings["webPage"];
+            List<SourceConsole> listSourceConsole = new List<SourceConsole>();
+            _driver.Navigate().GoToUrl(_baseUrl + "/OpsCenter/SourceConsole.aspx");
+            IReadOnlyCollection<IWebElement> tables = _driver.FindElements(By.CssSelector("table"));
+            foreach (ReadOnlyCollection<IWebElement> rows in from table in tables where table.GetAttribute("id").Contains("SourceGrid") select table.FindElements(By.CssSelector("tr")))
+            {
+                listSourceConsole.AddRange(rows.Select(row => new SourceConsole(row)).Where(sourceConsole => sourceConsole.IsInvalidRow()));
+            }
+            PrintInvalidRows(listSourceConsole);
+            Assert.AreEqual(0, listSourceConsole.Count);
+        }
+
+        private void PrintInvalidRows(List<SourceConsole> listSourceConsole)
+        {
+            foreach (var sourceConsole in listSourceConsole)
+            {
+                Debug.WriteLine(string.Format("Source:{0} \t \t Weight:{1}", sourceConsole.Source, sourceConsole.ApprovalWeight));
+            }
+        }
+    }
+}
