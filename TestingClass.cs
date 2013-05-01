f9a1d032c55c7deea724e7e8bc01be8bc2c2a3a9
Gene Ho <gho@merchantwarehouse.com>
2013-05-01 10:11:50 -0400

    refactored.  Simplified Linq, and use more detailed css selectors

--- TestingClass.cs
+++ TestingClass.cs
@@ -1,69 +1,64 @@
 ﻿using System.Diagnostics;
 using System.Configuration;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 
-namespace Smoking_Gun
-{
-    public class TestingClass
-    {
+namespace Smoking_Gun {
+    public class TestingClass {
         private IWebDriver _driver;
         private string _baseUrl;
-        
+
         [SetUp]
-        public void SetupTest()
-        {
+        public void SetupTest() {
             _driver = new InternetExplorerDriver();
             _baseUrl = ConfigurationManager.AppSettings["baseURL"];
         }
 
         [TearDown]
-        public void TeardownTest()
-        {
+        public void TeardownTest() {
             _driver.Quit();
         }
 
         [Test]
-        public void BadTest()
-        {
-            string url = _baseUrl + ConfigurationManager.AppSettings["webPage"];
-            _driver.Navigate().GoToUrl(_baseUrl + "/OpsCenter/SourceConsole.aspx");
-            IWebElement table = _driver.FindElement(By.CssSelector("table.specialClass"));
-            List<SourceConsole> listSourceConsole = table.FindElements(By.CssSelector("tr"))
-                                                    .Select(row => new SourceConsole(row))
-                                                    .Where(sourceConsole => sourceConsole.IsInvalidRow())
-                                                    .ToList();
-            PrintInvalidRows(listSourceConsole);
-            Assert.AreEqual(0, listSourceConsole.Count);
+        public void BadTest() {
+            string url = _baseUrl + "/OpsCenter/SourceConsole.aspx";
+
+            _driver.Navigate().GoToUrl(url);
+
+            var elements = _driver.FindElements(By.CssSelector("table.specialClass tr"));
+
+            var invalidRows = ( from webElement in elements
+                                let row = new SourceConsole(webElement)
+                                where row.IsInvalidRow() == true
+                                select row ).ToList();
+
+            printRows(invalidRows);
+
+            Assert.AreEqual(0, invalidRows.Count());
         }
 
         [Test]
-        public void BetterTest()
-        {
-            string url = _baseUrl + ConfigurationManager.AppSettings["webPage"];
-            List<SourceConsole> listSourceConsole = new List<SourceConsole>();
-            _driver.Navigate().GoToUrl(_baseUrl + "/OpsCenter/SourceConsole.aspx");
-            IReadOnlyCollection<IWebElement> tables = _driver.FindElements(By.Id("table"));
-            foreach (ReadOnlyCollection<IWebElement> rows in from table in tables 
-                                                             where table.GetAttribute("id").Contains("SourceGrid") 
-                                                             select table.FindElements(By.CssSelector("tr")))
-            {
-                listSourceConsole.AddRange(rows
-                                            .Select(row => new SourceConsole(row))
-                                            .Where(sourceConsole => sourceConsole.IsInvalidRow()));
-            }
-            PrintInvalidRows(listSourceConsole);
-            Assert.AreEqual(0, listSourceConsole.Count);
+        public void BetterTest() {
+            string url = _baseUrl + "/OpsCenter/SourceConsole.aspx";
+            
+            _driver.Navigate().GoToUrl(url);
+
+            var webElements = _driver.FindElements(By.CssSelector("table[id$='SourceGrid'] tr"));
+
+            var invalidRows = ( from element in webElements
+                                let row = new SourceConsole(element)
+                                where row.IsInvalidRow()
+                                select row ).ToList();
+
+            printRows(invalidRows);
+            Assert.AreEqual(1, invalidRows.Count);
         }
 
-        private void PrintInvalidRows(List<SourceConsole> listSourceConsole)
-        {
-            foreach (var sourceConsole in listSourceConsole)
-            {
+        private void printRows(IEnumerable<SourceConsole> sourceControlList) {
+            foreach ( var sourceConsole in sourceControlList ) {
                 Debug.WriteLine(string.Format("Source:{0} \t \t Weight:{1}", sourceConsole.Source, sourceConsole.ApprovalWeight));
             }
         }
